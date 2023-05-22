37f39d59f4c1ed2e6a7798e96893cfab97d36e00
Vamsi <vk784@snu.edu.in>
2023-05-22 22:29:08 +0530

    Change dark-mode and light-mode pictures

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 const img =document.getElementById('image')
+const logo = document.getElementById('logo')
+const options = document.getElementById('options')
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
         body.style.color = 'black';
         //body.style.transition = '2s';
-        //img.src = "reasources/light-mac.jpeg"
+        img.src = "reasources/light-bat.jpg"
         img.style.width = "50%"
+        logo.src='reasources/6.png'
     }else{
-        body.style.background = 'rgba(15,16,18,255)';
+        body.style.background = 'black';
         body.style.color = 'white';
         //body.style.transition = '2s';
-        img.src = "reasources/dark-mac.jpeg"
+        img.src = "reasources/dark.jpg"
+        logo.src='reasources/5.png'
     }
 });
