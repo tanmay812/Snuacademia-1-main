de3856431edd4b5335e6f2080731816521d19ec4
Vamsi <vk784@snu.edu.in>
2023-07-17 15:34:45 +0530

    Update Faqs

--- src/index.js
+++ src/index.js
@@ -280,82 +280,61 @@ const erpGen = () => {
     {
       question: "What is ERP?",
       answer:
-        "ERP (Enterprise Resource Planning) is a software system that integrates various business processes and functions to streamline operations and enhance efficiency.",
+        "The Shiv Nadar University ERP portal is a web-based application that provides students, faculty, and staff with access to a variety of university services. The portal includes features such as student self-service, faculty self-service, staff self-service, and parent-student portal.",
     },
     {
-      question: "What are the benefits of implementing an ERP system?",
+      question: "How to log in to the ERP?",
       answer:
-        "Some of the benefits of implementing an ERP system include improved productivity, better data visibility, enhanced decision-making, streamlined processes, and cost savings.",
+        "To log in to your ERP, you must enter your SNU net ID (e.g.- xy123) and password.",
     },
     {
-      question: "How does an ERP system work?",
+      question: "How to add courses to planner?",
       answer:
-        "An ERP system works by integrating data and processes across different departments or functions within an organization, allowing for centralized data storage, real-time information sharing, and efficient collaboration.",
+        "Upon logging into the ERP, navigate to the 'Academic Planning' section located in the left-hand menu bar, or directly access the 'Plan' option at the top of the page alongside other options like search and enroll. Proceed to click on 'Browse Catalog' and use the text box to input the first letter of the subject you desire to select a course from. All available subject options will be displayed, where you can then click the dropdown button to choose your desired course and click on 'Add Planner' to include it in your planner.",
     },
     {
-      question: "What modules are typically included in an ERP system?",
+      question: "Where to see your timetable from?",
       answer:
-        "Common modules in an ERP system include finance and accounting, human resources, inventory management, procurement, sales and marketing, production planning, and customer relationship management (CRM).",
+        "In the ERP system, locate the 'Enrollment' option on the left-hand side of the page, situated under the menu box. Click on the 'My Weekly Schedule' option to access your timetable for the current week.",
     },
     {
-      question: "How can ERP benefit educational institutions?",
+      question: "How to check GPA?",
       answer:
-        "ERP systems can benefit educational institutions by automating administrative tasks, managing student records, facilitating communication, improving resource allocation, and supporting data-driven decision-making.",
+        "In the ERP system, find the 'Enrollment' option on the left-hand side of the page, which is located under the menu box. Click on 'Term Statistics' and choose the semester for which you wish to check your GPA. Then, review your GPA for the selected semester.",
+    },
+    {
+      question:
+        "How to check the total credits completed and total credits left?",
+      answer:
+        'Find the "My Academics" option in the ERP system along with other options like "Enrol," "Plan," etc. near the top of the page. Then select "View My Advisement Report." This report will show the credits you have already earned as well as the credits you still need to finish.',
     },
   ];
   let fastrackFaqs = [
     {
       question: "What is Fastrack?",
       answer:
-        "Fastrack is a system used for applying for permission to leave the campus grounds temporarily.",
+        'To leave the campus for a day or longer, you must obtain approval from the warden. This approval is referred to as "fastrack."',
     },
     {
       question: "How do I apply for Fastrack?",
       answer:
-        "To apply for Fastrack, you need to log in to the Fastrack portal, fill out the required details, and submit your application.",
+        'Please access the SNULinks website and find the "login" option. Once logged in, you will see a variety of choices. Click on the "fastrack" option to access the form for requesting permission to leave the campus, whether it is for personal or official reasons.',
     },
     {
-      question: "What are the eligibility criteria for Fastrack?",
+      question: "What to do in case Fastrack is rejected?",
       answer:
-        "The eligibility criteria for Fastrack may vary depending on the university's policies. Typically, it may include factors such as academic standing, disciplinary record, and attendance.",
-    },
-    {
-      question: "How long does it take to get a Fastrack approval?",
-      answer:
-        "The time taken to receive a Fastrack approval can vary. It is recommended to submit your application well in advance to allow sufficient time for the approval process.",
-    },
-    {
-      question: "What should I do if my Fastrack application gets rejected?",
-      answer:
-        "If your Fastrack application gets rejected, you can reach out to the designated authority or department to understand the reasons for rejection and seek further guidance.",
+        "If your fastrack request is rejected for any reason, you have two options: You can review the form to see if there are any mistakes, correct them, and resubmit it for approval. Alternatively, if you need to leave urgently, you can directly approach the reception of your hostel and request an early approval from them.",
     },
   ];
   let hostelFaqs = [
     {
-      question: "What is hostel management?",
-      answer:
-        "Hostel management refers to the administration and supervision of residential facilities provided to students or individuals within an educational institution or other similar settings.",
+      question: "How to choose roommates?",
+      answer: "The option to remove roommates is no longer valid as of 2023",
     },
     {
-      question: "How do I apply for hostel accommodation?",
+      question: "How to select your room?",
       answer:
-        "To apply for hostel accommodation, you typically need to fill out a hostel application form provided by your educational institution. The form will require necessary details such as personal information, preferred room type, and any specific requirements.",
-    },
-    {
-      question: "What amenities are usually provided in hostels?",
-      answer:
-        "Hostels generally offer basic amenities such as beds, study desks, storage spaces, shared bathrooms, common areas, and sometimes additional facilities like recreational areas, laundry rooms, or dining halls.",
-    },
-    {
-      question: "What are the rules and regulations in hostels?",
-      answer:
-        "Hostels have specific rules and regulations that residents must adhere to, including guidelines related to curfew hours, visitor policies, noise restrictions, cleanliness, safety protocols, and other aspects to ensure a conducive living environment for all residents.",
-    },
-    {
-      question:
-        "What should I do if I have issues or complaints regarding hostel facilities?",
-      answer:
-        "If you have any issues or complaints regarding hostel facilities, it is recommended to contact the hostel management office or the designated authority responsible for addressing such concerns. They will assist you in resolving the problem or guiding you on the necessary steps to take.",
+        "Please visit the SNULinks website and locate the 'login' option. After logging in, you will be presented with various choices. Click on the 'Hostel Management System' option, and from there, find the 'Room Selection Request' option. During the room selection days assigned by the management, this selection will redirect you to a page where you can choose your room.",
     },
   ];
   let blackboardFaqs = [
@@ -365,24 +344,24 @@ const erpGen = () => {
         "Blackboard is an online learning management system (LMS) used by professors to provide course materials, including notes, assignments, announcements, and other resources to students in a digital format.",
     },
     {
-      question: "How do I access Blackboard?",
+      question: "How to log in blackboard?",
       answer:
-        "To access Blackboard, you need to log in to your educational institution's Blackboard portal using your assigned username and password. The portal is typically accessible through a web browser or a dedicated Blackboard mobile app.",
+        "To access the Blackboard platform at SNU, you have two options. Firstly, you can navigate to SNU links and search for the 'Blackboard' option on the website. Alternatively, you may directly search for 'SNU Blackboard' on the internet. Once you find the Blackboard portal, log in using your SNU email ID and password to gain access to the platform..",
     },
     {
-      question: "What kind of materials can I find on Blackboard?",
+      question: "How to add courses to favorites?",
       answer:
-        "On Blackboard, professors can upload various materials such as lecture notes, slides, reading materials, videos, quizzes, discussion boards, and other course-related resources. It serves as a centralized platform for accessing and managing course content.",
+        "After logging in to your Blackboard account, find the 'Courses' option located on the left-hand side of the screen and click on it. This action will display a list of all the courses you are currently enrolled in. To mark a course as a favorite, look for the star button situated on the right-hand side of each course heading. Click on the star button to add the course to your favorites list.",
     },
     {
       question: "How can I submit assignments on Blackboard?",
       answer:
-        "Blackboard provides features for students to submit their assignments electronically. Professors usually set up specific assignment submission areas within Blackboard where students can upload their files or enter text-based responses.",
+        "Normally, the link for submitting assignments can be found in the 'Activity Section' of the course page, along with the deadline for submission. If you are unable to locate the link there, you can follow these steps: First, click on the 'Courses' option on the left-hand side of the screen. Next, find the specific course for which you want to submit the assignment. Once you have selected the course, navigate to the 'Content Section' of the course page and look for the link to the assignment submission.",
     },
     {
-      question: "Can I communicate with my professors through Blackboard?",
+      question: "How to access content on blackboard?",
       answer:
-        "Yes, Blackboard offers communication tools such as email, discussion boards, and messaging features that allow students to interact with their professors. You can use these tools to ask questions, seek clarification, or engage in course-related discussions.",
+        "Usually, the most recent content is visible on the 'Activity Stream.' However, if you want to access all the content available for a particular course, follow these steps: Go to the 'Courses' section and choose the specific course you wish to view the content of. Look for the 'Content' section, typically located on the left-hand side of the screen. By accessing this section, you will be able to see all the content related to the selected course.",
     },
   ];
   document.getElementById("erpLink").addEventListener("click", function () {
